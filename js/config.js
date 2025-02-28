7b1be42f30ab42dc34f8cd9f9ed9cd42feea33f7
Yopp <ColbYopp@helenaagri.com>
2025-02-28 13:16:15 -0600

    added user pool and app client IDs

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_u5WfvW1hO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3hnjcucecd4b5s4h2bnmsq0je9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
